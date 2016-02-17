9154ad8227b971b8a2b636d7111080796ca3c0a6
Julien <bacquartj@gmail.com>
2016-02-17 13:23:09 -0800

    Changed visible from int to bool

--- src/test.c
+++ src/test.c
@@ -9,12 +9,13 @@
 typedef void (*fct_parcours_t)(xmlNodePtr);
 
 void print_tag(Tag tag){
-	printf("TAAAAAAAAAAAAAG %s = %s\n", tag.key,tag.val);
+	printf("Tag k: %s, v: %s\n", tag.key, tag.val);
 }
 
 void print_node(Node node){
+	// For lat et long we need a precision to the 7 decimal place
+	printf("Node id: %d free_tags=%d lat=%.7f lon=%.7f visible=%d\n", node.id, node.free_tags, node.lat, node.lon, node.visible);
 	int i;
-	printf("id=%d \n free_tags=%d \n lat=%f\n lon=%f \n", node.id, node.free_tags, node.lat, node.lon);
 	for(i = 0; i< node.free_tags; i++){
 		print_tag(node.tags[i]);
 	}
@@ -25,83 +26,137 @@ void parcours_prefixe(xmlNodePtr noeud, fct_parcours_t f) {
     int nodes_i=0;
     xmlNodePtr n;
     
+    // We traverse the xml tree looking at each node
     for (n = noeud->children; n != NULL; n = n->next) {
+	// Case node
 	if (!strcmp(n->name,"node")){
-		printf("************node found**************\n");
-		// RECUPERER LES ATTRIBUTS DU NOEUD
-		xmlAttr* attribute = n->properties;
+		
 		int node_id;
-		xmlNodePtr n_fils;
 		double node_lat;
 		double node_lon;
-		nodes[nodes_i].free_tags= 0;
-
+		bool node_visible;
+		
+		nodes[nodes_i].free_tags= 0;	// should be changed
+		
+		// We extract the attributes of a node
+		xmlAttr* attribute = n->properties;
 		while(attribute)
 		{
 			xmlChar* value = xmlNodeListGetString(n->doc, attribute->children, 1);
+			
 			if(!strcmp(attribute->name,"id")){
 				node_id= atoi(value);
 			}
-			else if(!strcmp(attribute->name,"lat")){
+			else if (!strcmp(attribute->name,"lat")){
 				node_lat= atof(value);	
 			}
-			else if(!strcmp(attribute->name,"lon")){
+			else if (!strcmp(attribute->name,"lon")){
 				node_lon = atof(value);
 			}
+			else if (!strcmp(attribute->name,"visible")){
+				// Check if the node is visible
+				if (strcmp(value,"true") == 0) {
+					node_visible = true;
+				} else {
+					node_visible = false;
+				}				
+			}
 			
-		  	//printf(" %s", attribute->name);
-		  	//printf("=%s ;", value);
-			
+			// Memory managment 
 		  	xmlFree(value); 
+			
 		 	attribute = attribute->next;
 		}
-		//printf("id = %d; lon =%f; lat=%f \n",node_id,node_lon,node_lat);
 		
-		//CHERCHER LES TAGS
+		// We extract the tags of a node
+		xmlNodePtr n_fils;
 		for (n_fils = n->children; n_fils != NULL; n_fils = n_fils->next) {
+
 			xmlAttr* attributes_fils = n_fils->properties;
+			
 			if (!strcmp(n_fils->name,"tag")){
 				char * key;
 				char * val;
-				//printf("************************************************************On traite le tag\n");
+				
 				while(attributes_fils)
 				{
 					xmlChar* value_fils = xmlNodeListGetString(n_fils->doc, attributes_fils->children, 1);
-					if(!strcmp(attributes_fils->name,"k")){
+					
+					if (!strcmp(attributes_fils->name,"k")){
+						// Key
 						key = malloc(strlen(value_fils)+1);
-						strcpy(key,  value_fils);
-						//printf("%s =",key);
-					}
-					else{
+						strcpy(key, value_fils);
+					} else {
+						// Value
 						val = malloc(strlen(value_fils)+1);
 						strcpy(val,  value_fils);
-						//printf("%s\n",val);
 					}
+					
+					// Memory managment 
 					xmlFree(value_fils);
+					
 		 			attributes_fils = attributes_fils->next;
 				}
+				
+				// should be changed
 				nodes[nodes_i].tags[nodes[nodes_i].free_tags].key= key;
 				nodes[nodes_i].tags[nodes[nodes_i].free_tags].val= val;
 				nodes[nodes_i].free_tags++;
 				
 			}
-		}	
+		}
+		
+		// should be changed
 		nodes[nodes_i].id= node_id;
 		nodes[nodes_i].lat= node_lat;
 		nodes[nodes_i].lon= node_lon;
+		nodes[nodes_i].visible = node_visible;
 	
 		print_node(nodes[nodes_i]);
 		nodes_i++;
 		
 	}
 
-	else if (!strcmp(n->name,"way")){
-		printf("on traite la way\n");
-	}
-	
-	else{
+// 	else if (!strcmp(n->name,"way")){
+// 		printf("************way found**************\n");
+// 		// We get the attributes of the way
+// 		xmlAttr* attribute = n->properties;
+// 		
+// 		int way_id;
+// 		xmlNodePtr n_fils;
+// 		double way_lat;
+// 		double way_lon;
+// 		
+// 		// Get attribute (id)
+// 		while(attribute)
+// 		{
+// 			xmlChar* value = xmlNodeListGetString(n->doc, attribute->children, 1);
+// 			if(!strcmp(attribute->name,"id")){
+// 				node_id= atoi(value);
+// 			}
+// 			else if(!strcmp(attribute->name,"lat")){
+// 				node_lat= atof(value);	
+// 			}
+// 			else if(!strcmp(attribute->name,"lon")){
+// 				node_lon = atof(value);
+// 			}
+// 			
+// 		  	//printf(" %s", attribute->name);
+// 		  	//printf("=%s ;", value);
+// 			
+// 		  	xmlFree(value); 
+// 		 	attribute = attribute->next;
+// 		}
+// 		//printf("id = %d; lon =%f; lat=%f \n",node_id,node_lon,node_lat);
+// 		
+// 		// Process nodes
+// 		
+// 		// Process tags
+// 	}
+// 	
+// 	else{
 		
-	}
+// 	}
        // f(n);
 
     }
