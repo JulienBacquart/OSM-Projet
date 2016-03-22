25f06ba64c867c04bba9491a4e9e9ac34a8d515a
Julien <bacquarj@gmail.com>
2016-03-22 22:35:16 +0100

    Changed signature of function drawRoad

--- include/graphics.h
+++ include/graphics.h
@@ -13,6 +13,6 @@
 
 void doPause();
 int drawLine(SDL_Renderer *renderer,int x1, int y1,int x2, int y2,int width,int r,int g, int b, int a);
-int drawRoad(SDL_Renderer *renderer,Node * nds,int width,int nbPoints,int r, int g, int b, int a,Node *h_nodes,Bounds *m_bds);
+int drawRoad(SDL_Renderer *renderer, Way *way, Node *h_nodes, Bounds *m_bds, int draw_width ,int r, int g, int b, int alpha);
 int drawMur(SDL_Renderer *renderer,Node * nds,int width,int nbPoints,int r, int g, int b, int a,Node *h_nodes,Bounds *m_bds);
 //int writeText(SDL_Renderer *renderer,char *text,int fontWidth,int x, int y,int width,int height,int r,int g,int b,double angle);
