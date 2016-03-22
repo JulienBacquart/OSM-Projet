f8e21b44a5d0e554e356bd4f9ed62421caffd54f
Julien <bacquarj@gmail.com>
2016-03-22 22:38:26 +0100

    Cleaned code. Added different type of roads

--- src/renderer.c
+++ src/renderer.c
@@ -1,108 +1,147 @@
 #include "../include/parsexml.h"
 //#include "../include/graphics.h"
 
-int main(int argc, char *argv[])
-{
-	if(argc != 2)
-	{
-		fprintf(stderr,"Usage: %s <file>.osm\n",argv[0]);
-		exit(0);
-	}
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+int main(int argc, char *argv[]) {
 	
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
-        return -1;
-    }
-
-	window = SDL_CreateWindow("OpenStreetMap - Renderer",40,-105,WIDTH, HEIGHT,SDL_WINDOW_SHOWN); //Création de la fenêtre
-    	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);//Création du renderer associé a la fenêtre
-	
-	SDL_SetRenderDrawColor(renderer,242,239,233,0);
-	SDL_RenderClear(renderer);
- 	SDL_RenderPresent(renderer);
-
-	/** Ici le code
-		
-			
-		
-
-
-
-	*/
-	xmlDocPtr doc;
-	xmlNodePtr racine;
-
 	if(argc > 1) {
+		xmlDocPtr doc;
+		xmlNodePtr racine;
+		printf("Start parsing %s...\n", argv[1]);
 		
 		// Ouverture du document
 		xmlKeepBlanksDefault(0); // Ignore les noeuds texte composant la mise en forme
 		doc = xmlParseFile(argv[1]);
-	if (doc == NULL) {
-		fprintf(stderr, "Document XML invalide\n");
-		return EXIT_FAILURE;
-	}
-	// Récupération de la racine
-	racine = xmlDocGetRootElement(doc);
-	if (racine == NULL) {
+		if (doc == NULL) {
+			fprintf(stderr, "Document XML invalide\n");
+			return EXIT_FAILURE;
+		}
+		
+		// Récupération de la racine
+		racine = xmlDocGetRootElement(doc);
+		if (racine == NULL) {
 			fprintf(stderr, "Document XML vierge\n");
 			xmlFreeDoc(doc);
 			return EXIT_FAILURE;
-	}
+		}
+		
 		// Parcours
 		Map map = parcours_prefixe(racine, afficher_noeud);
-		Way *w = NULL;		
 		
-		for(w=map.h_ways; w!=NULL; w=(Way*)(w->hh.next)){
-			printf(" id = %d \n", w->id);
-			printf(" nb_points = %d \n",w->nb_nds);
-			// CODE DEGUEULASSE ........................................................................................
-			int i=0;
-			int found= 0;
-			printf("nb_tags %d \n", w->nb_tags);
-			for(i=0; i < w->nb_tags; i++){
-				printf("%s \n", w->tags[i].key);
-				if(strcmp(w->tags[i].key,"highway")==0){
-					found =1;
-					break;
+		printf("Document parsed successfully, data extracted:\n");
+		print_map(map);
+		
+		// Create display window
+		SDL_Window *window;
+		SDL_Renderer *renderer;
+
+		if (SDL_Init(SDL_INIT_VIDEO) < 0){
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
+			return -1;
+		}
+
+		window = SDL_CreateWindow("OpenStreetMap - Renderer",40,-105,WIN_WIDTH, WIN_HEIGHT,SDL_WINDOW_SHOWN); //Création de la fenêtre
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);//Création du renderer associé a la fenêtre
+	
+		SDL_SetRenderDrawColor(renderer,242,239,233,0);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+		
+		// Draw ways
+		Way *w;
+		for(w=map.h_ways; w != NULL; w=w->hh.next) {			
+			int i;
+			
+			// Parcours tags
+			for(i = 0; i < w->nb_tags; i++){
+// 				print_tag(w->tags[i]);
+				
+				// The way is a road
+				if(strcmp(w->tags[i].key,"highway") == 0){
+					
+					// Primary road
+					if(strcmp(w->tags[i].val,"primary") == 0){
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 15, 255, 161, 144, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					// Tertiary road
+					if(strcmp(w->tags[i].val,"tertiary") == 0){
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 255, 255, 150, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					// residential road
+					else if(strcmp(w->tags[i].val,"residential") == 0){
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 255, 255, 255, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					// pedestrian road
+					else if(strcmp(w->tags[i].val,"pedestrian") == 0){
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 215, 215, 215, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					// service road
+					else if(strcmp(w->tags[i].val,"service") == 0){
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 195, 195, 195, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					// footway road
+					else if(strcmp(w->tags[i].val,"footway") == 0){
+						// Should be dashed line
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 0, 195, 195, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					// cycleway road
+					else if(strcmp(w->tags[i].val,"cycleway") == 0){
+						// Should be dotted line
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 0, 0, 195, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					// unclassified road
+					else if(strcmp(w->tags[i].val,"unclassified") == 0){
+						// Should be dashed line
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 195, 195, 0, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
+					
+					else {
+						printf("highway value:%s\n", w->tags[i].val); 
+						drawRoad(renderer, w, map.h_nodes, map.m_bds, 10, 195, 0, 0, 255);
+						SDL_RenderPresent(renderer);
+						break;
+					}
 				}
-				if(strcmp(w->tags[i].key,"building")==0){
-					found =2;
-					break;
-				}
-			}
-			if(found ==1){
-				drawRoad(renderer,w->nds,10,w->nb_nds,255, 255, 255, 255, map.h_nodes,map.m_bds);
-				SDL_RenderPresent(renderer);
-			}
-			else if(found==2){
-				drawMur(renderer,w->nds,10,w->nb_nds,255, 255, 255, 255, map.h_nodes,map.m_bds);
-				SDL_RenderPresent(renderer);
-			}
-			else{
-				drawRoad(renderer,w->nds,1,w->nb_nds,255, 0, 0, 255, map.h_nodes,map.m_bds);
-				SDL_RenderPresent(renderer);			
 			}
 		}
 		
-		print_map(map);
-		// Libération de la mémoire
+		doPause();
+		
+		// Libération de la mémoire		
 		xmlFreeDoc(doc);
-	}
-	else {
-		fprintf(stderr, "Name of the map file expected as argument\n");
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		
+	} else {
+		fprintf(stderr,"Usage: %s <file>.osm\n",argv[0]);
 		exit (EXIT_FAILURE);
-	 }
+	}
 	
-
-	doPause();
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
