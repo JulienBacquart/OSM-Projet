991e0236e247155b83d0cffc74df19adeddd36fc
Julien <bacquarj@gmail.com>
2016-03-23 16:37:15 +0100

    Added drawPolygon and drawBuilding. Changed drawRoad

--- include/graphics.h
+++ include/graphics.h
@@ -8,11 +8,12 @@
 #include "calcul.h"
 #include "data.h"
 
-#define WIDTH 800
-#define HEIGHT 800
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
 
 void doPause();
 int drawLine(SDL_Renderer *renderer,int x1, int y1,int x2, int y2,int width,int r,int g, int b, int a);
+int drawPolygon(SDL_Renderer *renderer, short *x_tab, short *y_tab, int nb_pts, int r, int g, int b, int a);
 int drawRoad(SDL_Renderer *renderer, Way *way, Node *h_nodes, Bounds *m_bds, int draw_width ,int r, int g, int b, int alpha);
-int drawMur(SDL_Renderer *renderer,Node * nds,int width,int nbPoints,int r, int g, int b, int a,Node *h_nodes,Bounds *m_bds);
+int drawBuilding(SDL_Renderer *renderer, Way *way, Node *h_nodes, Bounds *m_bds, int r, int g, int b, int alpha);
 //int writeText(SDL_Renderer *renderer,char *text,int fontWidth,int x, int y,int width,int height,int r,int g,int b,double angle);
